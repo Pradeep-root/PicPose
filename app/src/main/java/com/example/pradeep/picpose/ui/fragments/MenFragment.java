70fcf76e787c08f4e8c880536f7e370f914d6aaa
Pradeep <pradeepemc3@gmail.com>
2017-04-03 00:19:48 +0530

    work on fragment load data, and navigation design

--- app/src/main/java/com/example/pradeep/picpose/ui/fragments/MenFragment.java
+++ app/src/main/java/com/example/pradeep/picpose/ui/fragments/MenFragment.java
@@ -3,13 +3,22 @@ package com.example.pradeep.picpose.ui.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.pradeep.picpose.R;
+import com.example.pradeep.picpose.adapters.PoseAdapter;
+import com.example.pradeep.picpose.model.PoseModel;
+
+import java.util.ArrayList;
 
 public class MenFragment extends BaseFragment {
+
+    private RecyclerView recyclerView;
+    private PoseAdapter poseAdapter;
 
     public MenFragment() {
         // Required empty public constructor
@@ -25,6 +34,24 @@ public class MenFragment extends BaseFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_men, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        loadData();
+    }
+
+    private void loadData() {
+        poseAdapter = new PoseAdapter(new ArrayList<PoseModel>());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(poseAdapter);
+    }
+
+    private void init(View view) {
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewMen);
     }
 
     @Override
--- app/src/main/java/com/example/pradeep/picpose/ui/fragments/WomenFragment.java
+++ app/src/main/java/com/example/pradeep/picpose/ui/fragments/WomenFragment.java
@@ -2,14 +2,24 @@ package com.example.pradeep.picpose.ui.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.pradeep.picpose.R;
+import com.example.pradeep.picpose.adapters.PoseAdapter;
+import com.example.pradeep.picpose.model.PoseModel;
+
+import java.util.ArrayList;
 
 public class WomenFragment extends BaseFragment {
+
+    private PoseAdapter poseAdapter;
+    private RecyclerView recyclerView;
 
     public WomenFragment() {
         // Required empty public constructor
@@ -29,9 +39,27 @@ public class WomenFragment extends BaseFragment {
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        loadData();
+    }
+
+    @Override
     public void onAttach(Context context) {
         super.onAttach(context);
 
+    }
+
+    private void init(View view) {
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewWoMen);
+    }
+
+    private void loadData() {
+        poseAdapter = new PoseAdapter(new ArrayList<PoseModel>());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(poseAdapter);
     }
 
     @Override
